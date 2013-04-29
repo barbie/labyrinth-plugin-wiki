2b82340aa069c905dbc7a57167372033147c81a6
Barbie <barbie@missbarbell.co.uk>
2013-04-29 22:03:32 +0100

    META updates

--- lib/Labyrinth/Plugin/Wiki/Text.pm
+++ lib/Labyrinth/Plugin/Wiki/Text.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '1.03';
+$VERSION = '1.04';
 
 =head1 NAME
 
@@ -346,7 +346,7 @@ Miss Barbell Productions, L<http://www.missbarbell.co.uk/>
 
 =head1 COPYRIGHT & LICENSE
 
-  Copyright (C) 2002-2012 Barbie for Miss Barbell Productions
+  Copyright (C) 2002-2013 Barbie for Miss Barbell Productions
   All Rights Reserved.
 
   This module is free software; you can redistribute it and/or
